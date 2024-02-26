8dfff6a57d489ec1feef3f0b5bfa14ee9d4f5175
Thien Hao Le <lethienhao1996@gmail.com>
2024-02-26 14:38:31 +0700

    update

--- Hasaki-Music-Main-Pi.py
+++ Hasaki-Music-Main-Pi.py
@@ -11,8 +11,8 @@ import sys
 
 
 
-#location = ip.get()
-location = '14.241.238.138'
+location = ip.get()
+#location = '14.241.238.138'
 
 def hasaki_ringtone():
     os.environ["OMP_NUM_THREADS"]= '1'
